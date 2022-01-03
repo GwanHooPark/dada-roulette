284472e8e7edc46c2e40bc10913b3afa8ff44073
ghpark <ghpark@daou.co.kr>
2022-01-03 14:34:59 +0900

    test

---
+++ src/views/Roulette.vue
@@ -0,0 +1,14 @@
+<template>
+	<Roulette msg="다다 룰렛"></Roulette>
+</template>
+
+<script>
+import Roulette from '@/components/Roulette.vue';
+export default {
+	components: {
+		Roulette,
+	},
+};
+</script>
+
+<style></style>
